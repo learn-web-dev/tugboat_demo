dac7e629f7748b4a616089fad2cbf06868caa0e9
Ben Chavet <ben.chavet@lullabot.com>
2018-01-26 10:04:50 -0600

    Create a tugboat settings.php

---
+++ docroot/sites/default/tugboat.settings.php
@@ -0,0 +1,12 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'demo',
+  'username' => 'tugboat',
+  'password' => 'tugboat',
+  'prefix' => '',
+  'host' => 'mysql',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['install_profile'] = 'standard';
